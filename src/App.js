fc104a7d3772adf990fe29b0ba059cdd247a59b7
Icoriadb <icoriadebernardi@gmail.com>
2023-08-28 19:34:05 -0300

    about me

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-// import './App.css';
+import './App.css';
 import Header from './Components/Header/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Element } from "react-scroll";
+import AboutMe from './Components/About me/About';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Header></Header>
+      <Element name="home" className="element">
+          <Header />
+      </Element>
+      <AboutMe></AboutMe>
       </header>
     </div>
   );
