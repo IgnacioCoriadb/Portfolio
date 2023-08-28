83332d60f8183387a87fb4be560834814bb79a68
Icoriadb <icoriadebernardi@gmail.com>
2023-08-28 20:47:42 -0300

    header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,11 @@
 import styles from "./Header.module.css";
+import NavBar from '../Navbar/Navbar';
 
 const Header = () => {
     return (
       <div className={styles["header-wraper"]}>
+        <NavBar></NavBar>
+
         <div className={styles["main-info"]}>
           <h1 className={styles["h1-style"]}>Ignacio Coria de Bernardi</h1>
           <p className={styles["header-p"]}>Técnico en Desarrollo de Aplicaciones Web y Programador Full Stack</p>
