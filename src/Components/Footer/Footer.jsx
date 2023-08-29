4de54dadf0030dab8668d0b9f8f425870e1cffc7
Icoriadb <icoriadebernardi@gmail.com>
2023-08-28 23:33:24 -0300

    url redes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,8 +8,8 @@ const Footer = ()=>{
         <div className='container'>
             <footer className={ `${style["footer"]}`}>
                 <div className={style['social']}>
-                    <a href="https://www.linkedin.com/in/ignacio-coria-de-bernardi/"  target="_blank" rel="noopener noreferrer"><i className={`bi bi-github ${style['footer-icon']}` }></i></a>
-                    <a href="https://github.com/IgnacioCoriadb" target="_blank" rel="noopener noreferrer"><i className={`bi bi-linkedin ${style['footer-icon']}` }></i></a>
+                    <a href="https://github.com/IgnacioCoriadb"  target="_blank" rel="noopener noreferrer"><i className={`bi bi-github ${style['footer-icon']}` }></i></a>
+                    <a href="https://www.linkedin.com/in/ignacio-coria-de-bernardi/" target="_blank" rel="noopener noreferrer"><i className={`bi bi-linkedin ${style['footer-icon']}` }></i></a>
                     <a href="mailto:ignaciocoriadb@gmail.com" target="_blank" rel="noopener noreferrer"><i className={`bi bi-envelope ${style['footer-icon']}`}></i></a>
                 </div>
             </footer>
